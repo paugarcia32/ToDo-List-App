1bc8d75546cc718733a8119b4add0526da5699df
paugarcia32 <paugarcia32@gmail.com>
2024-12-26 13:44:27 +0100

    todos_repository testing updated

--- packages/todos_repository/test/src/todos_repository_test.dart
+++ packages/todos_repository/test/src/todos_repository_test.dart
@@ -8,6 +8,8 @@ class MockTodosApi extends Mock implements TodosApi {}
 
 class FakeTodo extends Fake implements Todo {}
 
+class FakeTag extends Fake implements Tag {}
+
 void main() {
   group('TodosRepository', () {
     late TodosApi api;
@@ -31,8 +33,20 @@ void main() {
       ),
     ];
 
+    final tags = [
+      Tag(
+        id: '1',
+        title: 'Work',
+      ),
+      Tag(
+        id: '2',
+        title: 'Personal',
+      ),
+    ];
+
     setUpAll(() {
       registerFallbackValue(FakeTodo());
+      registerFallbackValue(FakeTag());
     });
 
     setUp(() {
@@ -46,6 +60,14 @@ void main() {
       when(
         () => api.completeAll(isCompleted: any(named: 'isCompleted')),
       ).thenAnswer((_) async => 0);
+
+      when(() => api.getTags()).thenAnswer((_) => Stream.value(tags));
+      when(() => api.saveTag(any())).thenAnswer((_) async {});
+      when(() => api.deleteTag(any())).thenAnswer((_) async {});
+      when(() => api.getTodosByTag(any())).thenAnswer((invocation) async {
+        final tagId = invocation.positionalArguments.first as String;
+        return todos.where((todo) => todo.tagIds.contains(tagId)).toList();
+      });
     });
 
     TodosRepository createSubject() => TodosRepository(todosApi: api);
@@ -122,6 +144,82 @@ void main() {
         expect(subject.completeAll(isCompleted: true), completes);
 
         verify(() => api.completeAll(isCompleted: true)).called(1);
+      });
+    });
+
+    group('getTags', () {
+      test('makes correct api request', () {
+        final subject = createSubject();
+
+        expect(
+          subject.getTags(),
+          isNot(throwsA(anything)),
+        );
+
+        verify(() => api.getTags()).called(1);
+      });
+
+      test('returns stream of current list tags', () {
+        expect(
+          createSubject().getTags(),
+          emits(tags),
+        );
+      });
+    });
+
+    group('saveTag', () {
+      test('makes correct api request', () {
+        final newTag = Tag(
+          id: '3',
+          title: 'Urgent',
+        );
+
+        final subject = createSubject();
+
+        expect(subject.saveTag(newTag), completes);
+
+        verify(() => api.saveTag(newTag)).called(1);
+      });
+    });
+
+    group('deleteTag', () {
+      test('makes correct api request', () {
+        final subject = createSubject();
+
+        expect(subject.deleteTag(tags[0].id), completes);
+
+        verify(() => api.deleteTag(tags[0].id)).called(1);
+      });
+    });
+
+    group('getTodosByTag', () {
+      test('makes correct api request', () async {
+        final subject = createSubject();
+        final tagId = '1';
+
+        final todosConTag = [
+          todos[0],
+          todos[1],
+        ];
+
+        when(() => api.getTodosByTag(tagId)).thenAnswer((_) async => todosConTag);
+
+        final result = await subject.getTodosByTag(tagId);
+
+        expect(result, equals(todosConTag));
+        verify(() => api.getTodosByTag(tagId)).called(1);
+      });
+
+      test('returns empty list when no todos have the tag', () async {
+        final subject = createSubject();
+        final tagId = '999';
+
+        when(() => api.getTodosByTag(tagId)).thenAnswer((_) async => []);
+
+        final result = await subject.getTodosByTag(tagId);
+
+        expect(result, isEmpty);
+        verify(() => api.getTodosByTag(tagId)).called(1);
       });
     });
   });
